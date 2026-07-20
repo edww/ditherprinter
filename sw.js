808e7f3ca57494d5e05b8da69c45315208b23a4e
edww <37853188+edww@users.noreply.github.com>
2026-07-21 00:12:35 +0200

    Refresh app shell cache for sticky preview layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dither-printer-alpha-0.1';
+const CACHE = 'dither-printer-alpha-2';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
